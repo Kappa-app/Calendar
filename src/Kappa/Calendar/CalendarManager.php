84f18b3c8292cf58056f2f14d396d0cc8c2fd326
Budry <zarubaondra@gmail.com>
2013-09-06 12:20:11 +0200

    Added base manager

---
+++ src/Kappa/Calendar/CalendarManager.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file is part of the Kappa/Calendar package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+
+namespace Kappa\Calendar;
+
+use Nette\Object;
+
+/**
+ * Class CalendarManager
+ * @package Kappa\Calendar
+ */
+class CalendarManager extends Object
+{
+	
+}
